fec88cf8129b073c2d4a1e300c633cfffafe884b
vukedd <vukedd@gmail.com>
2023-12-19 17:16:50 +0100

    [add,edit] User edit modal, Minor design changes

--- Petshop/korisnici.js
+++ Petshop/korisnici.js
@@ -4,6 +4,9 @@ let FirebaseURL = "https://pet-shop-fff9d-default-rtdb.europe-west1.firebasedata
 // Promenljiva u koju cemo dodati sve elemente iz jsona.
 let korisnici = {};
 
+// Ovde cu nakon svake iteracije sacuvati id korisnika.
+var trenutno;
+
 GetKorisnici();
 
 // Funkcija uz pomoc koje cemo izvuci sve korisnike iz json file-a.
@@ -18,9 +21,10 @@ function GetKorisnici() {
                 // JSON file pretvaramo u Javascript array
                 korisnici = JSON.parse(request.responseText);
 
-                // Izvrsavaom iteraciju kroz sve korisnike u /korisnici.json //
-                for (let id in korisnici) {
+                // Izvrsavamo iteraciju kroz sve korisnike u /korisnici.json //
+                for (var id in korisnici) {
                     let korisnik = korisnici[id];
+                    trenutno = id;
                     appendKorisniciRed("korisnicitbod", korisnik);
                 }
             } else {
@@ -32,6 +36,8 @@ function GetKorisnici() {
     request.open("GET", FirebaseURL + "/korisnici.json");
     request.send();
 }
+
+
 
 
 // ---------- POMOCNE FUNKCIJE ------------- //
@@ -77,21 +83,40 @@ function appendKorisniciRed(tBody, korisnik) {
     TelefonTd.innerText = korisnik.telefon;
     KorisniciRed.appendChild(TelefonTd);
 
+    // olovka
     let editTd = document.createElement("td");
     editTd.style.paddingLeft = "5px"
-    editTd.innerHTML = 
-    '<a href="#" id="izmenatabla"><i class="bi bi-pencil-fill"></i></a>'
+    // link u kom se nalazi ikonica olovka
+    let novaOlovka = document.createElement("a");
+    novaOlovka.setAttribute("id", "izmeniTabla");
+    novaOlovka.setAttribute("href", "#")
+    novaOlovka.setAttribute("data-toggle", "modal");
+    novaOlovka.setAttribute("data-target", "#exampleModalCenter2");
+    novaOlovka.setAttribute("data-dismiss", "modal")
+    novaOlovka.addEventListener("click", () => {
+        console.log("Hello world", korisnik)
+    })
+    // ikonica olovke 
+    let olovka = document.createElement("i");
+    olovka.setAttribute("class", "bi bi-pencil-fill");
+    // ikonicu dodajem u link
+    novaOlovka.appendChild(olovka);
+    // link dodajem u tD.
+    editTd.appendChild(novaOlovka);
+    // 
     KorisniciRed.appendChild(editTd);
 
+    // kanta
     let deleteTd = document.createElement("td");
     deleteTd.style.paddingLeft = "5px"
-    deleteTd.innerHTML = 
-    '<a onclick="ukloniKorisnika()" id="uklonitabla"><i class="bi bi-trash3-fill"></i></a>'
+    let novaKanta = document.createElement("a");
+    novaKanta.setAttribute("id", "ukloniTabla");
+    novaKanta.setAttribute("href", "#");
+    let kanta = document.createElement("i");
+    kanta.setAttribute("class","bi bi-trash3-fill");
+    novaKanta.appendChild(kanta);
+    deleteTd.appendChild(novaKanta);
     KorisniciRed.appendChild(deleteTd);
 
     document.getElementById(tBody).appendChild(KorisniciRed);
-}
-
-function ukloniKorisnika() {
-
-}
+} 
